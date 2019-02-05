f43ed19b2efa55404947b763664407f51ab61e4e
Silavsale <silavsale@gmail.com>
2019-02-05 01:01:45 -0500

    add links and images

---
+++ script/index.js
@@ -0,0 +1,14 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+console.log("JavaScript is amazing!");
+
+function openInNewTab() {
+    let url = 'http://oddix.games/';
+    let win = window.open(url, '_blank');
+    win.focus();
+}
+
+// href="http://google.com"
